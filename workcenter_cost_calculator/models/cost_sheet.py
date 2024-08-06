dc1cd57a5ad6856ed2bafb6aba87c9933a7cdfca
Malinda <pkgmalinda@gmail.com>
2024-08-07 02:45:17 +0530

    Date field

--- workcenter_cost_calculator/models/cost_sheet.py
+++ workcenter_cost_calculator/models/cost_sheet.py
@@ -6,7 +6,8 @@ class CostSheet(models.Model):
     _description = 'Cost Sheet'
 
     name = fields.Char(string='Name', required=True)
-    date_field = fields.Date(string='Date')
+    date_field = fields.Date(string='Date', help='Date of Cost Sheet Issued',
+                             default=fields.Date.today())
     total_cost = fields.Float(string='Total Cost', compute='_compute_total_cost', store=True)
     line_ids = fields.One2many('cost.sheet.line', 'cost_sheet_id', string='Cost Sheet Lines')
 
