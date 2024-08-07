564d521a0b29d10f8e87dae6b9a6ea36b09bda48
Malinda <pkgmalinda@gmail.com>
2024-08-07 11:16:03 +0530

    notebook feature

--- workcenter_cost_calculator/models/cost_sheet.py
+++ workcenter_cost_calculator/models/cost_sheet.py
@@ -6,10 +6,10 @@ class CostSheet(models.Model):
     _description = 'Cost Sheet'
 
     name = fields.Char(string='Name', required=True)
-    date_field = fields.Date(string='Date', help='Date of Cost Sheet Issued',
-                             default=fields.Date.today())
+    date_field = fields.Date(string='Date')
     total_cost = fields.Float(string='Total Cost', compute='_compute_total_cost', store=True)
     line_ids = fields.One2many('cost.sheet.line', 'cost_sheet_id', string='Cost Sheet Lines')
+    description = fields.Text(string='Description')  # Assuming you need this field
 
     @api.depends('line_ids.subtotal')
     def _compute_total_cost(self):
