46e671680467e94d05752bf62bbe9a2ae1310416
Malinda <pkgmalinda@gmail.com>
2024-08-08 10:15:14 +0530

    API Controller for Cost Sheets. Append cost sheet line details

--- workcenter_cost_calculator/controllers/cost_sheet_controller.py
+++ workcenter_cost_calculator/controllers/cost_sheet_controller.py
@@ -16,7 +16,21 @@ class CostSheetController(http.Controller):
     def get_all_cost_sheets(self, **kwargs):
         try:
             cost_sheets = request.env['cost.sheet'].sudo().search([])
-            cost_sheets_data = cost_sheets.read(['name', 'date_field', 'total_cost'])
+            cost_sheets_data = []
+            for sheet in cost_sheets:
+                sheet_data = sheet.read(['name', 'date_field', 'total_cost'])[0]
+                line_data = []
+                for line in sheet.line_ids:
+                    line_data.append({
+                        'id': line.id,
+                        'cost_sheet_id': line.cost_sheet_id.id,
+                        'work_center_id': line.work_center_id.id,
+                        'hours_spent': line.hours_spent,
+                        'subtotal': line.subtotal,
+                    })
+                sheet_data['line_ids'] = line_data
+                cost_sheets_data.append(sheet_data)
+
             return request.make_response(json.dumps(cost_sheets_data, default=date_handler), 
                                          headers=[('Content-Type', 'application/json')])
         except Exception as e:
@@ -30,8 +44,18 @@ class CostSheetController(http.Controller):
             if not cost_sheet:
                 return request.make_response(json.dumps({'error': 'Cost sheet not found'}),
                                              headers=[('Content-Type', 'application/json')], status=404)
-            cost_sheet_data = cost_sheet.read(['name', 'date_field', 'total_cost'])
-            return request.make_response(json.dumps(cost_sheet_data[0], default=date_handler), 
+            cost_sheet_data = cost_sheet.read(['name', 'date_field', 'total_cost'])[0]
+            line_data = []
+            for line in cost_sheet.line_ids:
+                line_data.append({
+                    'id': line.id,
+                    'cost_sheet_id': line.cost_sheet_id.id,
+                    'work_center_id': line.work_center_id.id,
+                    'hours_spent': line.hours_spent,
+                    'subtotal': line.subtotal,
+                })
+            cost_sheet_data['line_ids'] = line_data
+            return request.make_response(json.dumps(cost_sheet_data, default=date_handler), 
                                          headers=[('Content-Type', 'application/json')])
         except Exception as e:
             return request.make_response(json.dumps({'error': str(e)}),
